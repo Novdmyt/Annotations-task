6aa75304e0fe92eede8244e7d1317519f738b637
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-08-17 10:16:59 +0300

    Add Target and fix code

--- src/app/Product.java
+++ src/app/Product.java
@@ -1,12 +1,15 @@
 package app;
 
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 
-@Retention()
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Product {
     String name() default "unknown";
-    int quota() default;
+    int quota() default 0;
 }
